ad49a3892ffee423a3bed4d8f40abc03d8316c63
zhasy999 <zhasy99@mail.ru>
2020-11-06 22:06:30 +0600

    hys

--- src/main/java/com/example/demo/BrandApplication.java
+++ src/main/java/com/example/demo/BrandApplication.java
@@ -2,8 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrix
+@EnableHystrixDashboard
 public class BrandApplication {
 
 	public static void main(String[] args) {
